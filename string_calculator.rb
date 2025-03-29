21c8d5d6794a350bbafca7b39626110c7d443625
Rahul Purohit <rahulpuroht@gmail.com>
2025-03-29 17:49:28 +0530

    updated class as per spec for basic number addition

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,7 +1,23 @@
 class StringCalculator
 
+  
+
   def add(numbers)
     result=0
+    validate(numbers)
     return result if numbers.empty?
+    list = sanitize_input(numbers)
+    result = list.sum
+    result
   end
+
+  private
+
+    def validate(n)
+      raise ArgumentError unless n.is_a?(String)
+    end
+
+    def sanitize_input(str)
+      str.split(",").map(&:to_i)
+    end
 end
