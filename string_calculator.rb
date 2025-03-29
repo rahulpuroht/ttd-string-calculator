f926c99c5aa49ea5f94cf54f052e9d056b35d318
Rahul Purohit <rahulpuroht@gmail.com>
2025-03-29 18:32:56 +0530

    added check_delimitier for spec

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,13 +1,13 @@
 class StringCalculator
 
-  
+  DEFAULT_DELIMITERS = /[\n,]/
+  CUSTOM_DELIMITERS = /\/\/(.){0,1}\n/
 
   def add(numbers)
     result=0
     validate(numbers)
     return result if numbers.empty?
-    list = sanitize_input(numbers)
-    result = list.sum
+    result = sanitize_input(numbers).sum
     result
   end
 
@@ -18,6 +18,18 @@ class StringCalculator
     end
 
     def sanitize_input(str)
-      str.split(",").map(&:to_i)
+      list = check_delimitier(str).map(&:to_i)
+    end
+
+    def check_delimitier(str)
+      num_list = []
+      matches = CUSTOM_DELIMITERS.match(str)
+      if matches
+        delimiter = matches[1]
+        str_ith_delimiter = str.split(/[\n]/)[1]
+        numbers_list = str_ith_delimiter.split(delimiter)
+      else
+        numbers_list = str.split(DEFAULT_DELIMITERS)
+      end
     end
 end
